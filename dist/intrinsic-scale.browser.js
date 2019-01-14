0df2018604aaf73519b6897697fb38ef251dd71c
Isaac Torres <irt007@gmail.com>
2019-01-14 17:37:25 -0500

    Removing unnecessary package, babelrc and reverting dist changes

--- dist/intrinsic-scale.browser.js
+++ dist/intrinsic-scale.browser.js
@@ -1,2 +1,2 @@
 /*! npm.im/intrinsic-scale */
-!function(i){"use strict";function n(i){return function(n,t,c,r){var e=c/r,s=n/t,u=n,a=t;return(i?e>s:e<s)?a=u/e:u=a*e,{width:u,height:a,x:(n-u)/2,y:(t-a)/2}}}var t=n(!0),c=n(!1);i.contain=t,i.cover=c}(this.intrinsicScale=this.intrinsicScale||{});
+!function(i){"use strict";function n(i){return function(n,t,c,r){var e,s,u=c/r,a=n/t;return(i?u>a:u<a)?(e=n,s=e/u):(s=t,e=s*u),{width:e,height:s,x:(n-e)/2,y:(t-s)/2}}}var t=n(!0),c=n(!1);i.contain=t,i.cover=c}(this.intrinsicScale=this.intrinsicScale||{});
